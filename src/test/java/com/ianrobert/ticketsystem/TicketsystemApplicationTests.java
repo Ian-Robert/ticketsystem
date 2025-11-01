a917a7f68d832d871494fb741bb69a371eab1a90
Ian <adrielrobert760@gmail.com>
2025-11-01 18:24:28 -0400

    Project mostly initialized

---
+++ src/test/java/com/ianrobert/ticketsystem/TicketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianrobert.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
